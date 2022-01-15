4990af6d9c346b5766335455ad84ca3844c795e3
ShrutiKoli19 <85976870+ShrutiKoli19@users.noreply.github.com>
2022-01-15 18:42:21 +0530

    Update rotate-an-array.cpp

--- week1_assignment/rotate-an-array.cpp
+++ week1_assignment/rotate-an-array.cpp
@@ -10,7 +10,7 @@ class solution{
         }
     }
 
-    void rotateArr(int arr[], int , int n){
+    void rotateArr(int arr[], int d, int n){
         reverse(arr,0,d-1);
         reverse(arr, d,n-1);
         reverse(arr,0,n-1);
